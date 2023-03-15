6ef3fb1e9d603f254cbe2b5266fda76df0ffb8b4
Alexey_Volkov <alexiy87@inbox.ru>
2023-03-15 16:00:13 +0300

    feat:add a method for item 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
-﻿
+﻿string[] InitArray()
+{
+    string[] array = new string[4];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine("Введите текст для массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+
 
 
 
